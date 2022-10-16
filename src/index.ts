23fc8086431f67f3801c6c7f3ab1bbf6161483e4
Hassan Rabbi <hassan_rabbi@live.com>
2022-10-16 08:09:13 +0600

    proxy or static client serve

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Server started...')
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+
+export const serve = (
+  filename: string,
+  port: number,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app = express();
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: `http://localhost:3000`,
+        logLevel: "silent",
+      })
+    );
+  } else {
+    const clientPath = require.resolve("codepunk-client/build/index.html");
+    app.use(express.static(path.dirname(clientPath)));
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
+};
